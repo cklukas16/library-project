7d631dddbfb47f93fab5b086e17e80b421c7c750
dumengkun <dumengkun@gmail.com>
2023-11-03 01:04:54 -0500

    CRUD

---
+++ library/src/app/user-detail/user-detail.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { UserService } from '../user.service';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css']
+})
+export class UserDetailComponent implements OnInit {
+  user: User | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+  
+  getUser(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.userService.getUser(id)
+      .subscribe(user => this.user = user);
+  }
+
+  save(): void {
+    if (this.user) {
+      this.userService.updateUser(this.user).subscribe(() => this.goBack());
+    }
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
