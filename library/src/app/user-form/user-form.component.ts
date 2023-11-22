c5fa1525af65d6c84c78517165b90332d5607bab
noviceCoder0324 <88302674+noviceCoder0324@users.noreply.github.com>
2023-11-21 21:22:16 -0600

    fix refresh bug and return book bug

--- library/src/app/user-form/user-form.component.ts
+++ library/src/app/user-form/user-form.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { UserService } from '../shared/user.service';
 import { BookService } from '../shared/book.service';
-import { Borrow, User } from '../models/user';
+import { User } from '../models/user';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-user-form',
@@ -13,32 +14,21 @@ export class UserFormComponent implements OnInit {
   
   user: User | undefined;
 
+
   constructor(
     public auth: AngularFireAuth,
     private userService: UserService,
-    private bookService: BookService
+    private bookService: BookService,
+    private _location: Location
   ){ }
 
   ngOnInit(): void {
+    //to fix the refresh problem
+    if (!this.userService.currentUser) {
+      this._location.back();
+    }
     this.user = this.userService.currentUser;
-
-    // let uid = '';
-    // this.auth.user.subscribe((userLog) => {
-    //   uid = userLog?.email as string
-    //   this.getUser(uid);
-    // });
   }
-
-  // useless code after we have currentUser in userService
-  // getUser(uid: string): void {
-  //   if (this.userService.currentUser) {
-  //     this.user = this.userService.currentUser;
-  //     return;
-  //   }
-  //   this.userService.getUser(uid).subscribe(user => {
-  //     this.user = user;
-  //   });
-  // }
 
   //return book
   returnBook(id:number): void{
